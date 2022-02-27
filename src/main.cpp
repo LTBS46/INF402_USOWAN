bcc0260b242808e91703fe1e3d95d354a284d25b
LTBS46 <zyvon38@gmail.com>
2022-02-27 14:47:32 +0100

    Setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
-int main(int argc, char **argv) {
-  
+
+/// main function
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        if (argc == 1) {
+            /// pas d'argument
+        } else {
+            /// trop >1
+        }
+    } else {
+        /// argv[1] est le nom du fichier a résoudre
+    }
 }
