146c94454585f0abdfcde25d18e811e636d89127
LTBS46 <zyvon38@gmail.com>
2022-02-28 22:58:46 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 using std::cerr;            // flux d'erreur (stderr)
 using std::endl;            // symbole de fin de ligne
+using std::exception;
 using std::ifstream;        // type fichier en lecture
 using std::string;          // type chaine de caractere
 
@@ -14,7 +15,7 @@ int main(int argc, char** argv) try {
         if (argc == 1) {
             /// pas d'argument
             cerr << "il n'y a pas de fichier a résoudre" << endl;
-            return -1.
+            return -1;
         } else {
             /// trop >1
             cerr << "il y a trop d'argument : " << (argc - 1) << endl;
@@ -22,14 +23,14 @@ int main(int argc, char** argv) try {
         }
     } else {
         /// argv[1] est le nom du fichier a résoudre
-       
+
         /* nom du fichier */
         string file_name = argv[1];
-        
+
         /* pas besoin de le fermer il sera détruit en sorti de scope */
         ifstream input = ifstream(file_name);
     }
-} catch(exception & e) {
-    cerr << "erreur : \"" << e.what() << "\" caught"<< endl;
+} catch (exception& e) {
+    cerr << "erreur : \"" << e.what() << "\" caught" << endl;
     return -2;
 }
