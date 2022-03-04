a2618109ea406717bbe974ba53ba3b0e31393c0d
Baptiste <40568322+LTBS46@users.noreply.github.com>
2022-03-04 13:38:36 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 #include "common.hpp"
 
-#include <exception>
-#include <fstream>
-#include <iostream>
-#include <string>
+#include <exception>        // exception
+#include <filesystem>       // is_regular_file, path
+#include <fstream>          // ifstream
+#include <iostream>         // cerr, endl
+#include <string>           // string
 
-using std::cerr;            // flux d'erreur (stderr)
-using std::endl;            // symbole de fin de ligne
-using std::exception;
-using std::ifstream;        // type fichier en lecture
-using std::string;          // type chaine de caractere
+using std::cerr;                        // flux d'erreur (stderr)
+using std::cin;                         // flux d'entrée (stdin)
+using std::cout;                        // flux de sortie (stdout)
+using std::endl;                        // symbole de fin de ligne
+using std::exception;                   // la class de base pour les erreur
+using std::filesystem::is_regular_file; // teste si le fichier un fichier normal
+using std::filesystem::path;            // le type chemin de fichier
+using std::ifstream;                    // type fichier en lecture
+using std::string;                      // type chaine de caractere
 
 /// main function
 int main(int argc, char** argv) try {
@@ -27,8 +32,14 @@ int main(int argc, char** argv) try {
         /// argv[1] est le nom du fichier a résoudre
 
         /* nom du fichier */
-        string file_name = argv[1];
-
+        path file_name = path(string(argv[1]));
+        
+        /* on regarde si il s'agit d'un fichier normal */
+        if(! is_regular_file(file_name)) {
+            cerr << "le chemin indiqué n'est pas un fichier." << endl;
+            return -1;
+        }
+        
         /* pas besoin de le fermer il sera détruit en sorti de scope */
         ifstream input = ifstream(file_name);
     }
